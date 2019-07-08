d1b078178ae196a50327b552c3c1551c125b9a6b
Anurag-14 <tiwarianurag1993.at@gmail.com>
2019-07-08 14:08:11 +0530

    razor-pay-front-end-sdk added

---
+++ build/precache-manifest.db8adcef7082f7d4be8fa5e79b9d327a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e131139451e9e0da42399bcb31bacfaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "ad61eee554333884eb23",
+    "url": "/static/js/main.210a1209.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RazorPay from './RazorPay';
+ReactDOM.render(<RazorPay />, document.getElementById('root'));
+
